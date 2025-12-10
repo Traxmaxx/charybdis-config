647dacef4665d1c74d3248845cf0556936810716
Traxmaxx <320272+Traxmaxx@users.noreply.github.com>
2025-12-10 23:18:56 +0100

    accumulate trackball movement until activating pointer layer

--- charybdis/4x6/keymaps/vial/keymap.c
+++ charybdis/4x6/keymaps/vial/keymap.c
@@ -38,8 +38,15 @@ static uint16_t auto_pointer_layer_timer = 0;
 #    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS
 
 #    ifndef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD
-#        define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD 8
+#        define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD 20
 #    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD
+
+#    ifndef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_WINDOW_MS
+#        define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_WINDOW_MS 100
+#    endif // CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_WINDOW_MS
+
+static uint16_t accumulated_movement = 0;
+static uint16_t accumulation_timer = 0;
 
 #    ifdef RGB_MATRIX_ENABLE
 static uint8_t saved_rgb_mode = 0;
@@ -127,17 +134,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #ifdef POINTING_DEVICE_ENABLE
 #    ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    if (abs(mouse_report.x) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD || abs(mouse_report.y) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD) {
-        if (auto_pointer_layer_timer == 0) {
-            layer_on(LAYER_POINTER);
-#        ifdef RGB_MATRIX_ENABLE
-            saved_rgb_mode = rgb_matrix_get_mode();
-            saved_rgb_hsv = rgb_matrix_get_hsv();
-            rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
-            rgb_matrix_sethsv_noeeprom(HSV_GREEN);
-#        endif // RGB_MATRIX_ENABLE
+    if (mouse_report.x != 0 || mouse_report.y != 0) {
+        if (accumulation_timer == 0 || TIMER_DIFF_16(timer_read(), accumulation_timer) >= CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_WINDOW_MS) {
+            accumulated_movement = 0;
+            accumulation_timer = timer_read();
         }
-        auto_pointer_layer_timer = timer_read();
+        accumulated_movement += abs(mouse_report.x) + abs(mouse_report.y);
+
+        if (accumulated_movement >= CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD) {
+            if (auto_pointer_layer_timer == 0) {
+                layer_on(LAYER_POINTER);
+#        ifdef RGB_MATRIX_ENABLE
+                saved_rgb_mode = rgb_matrix_get_mode();
+                saved_rgb_hsv = rgb_matrix_get_hsv();
+                rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+                rgb_matrix_sethsv_noeeprom(HSV_GREEN);
+#        endif // RGB_MATRIX_ENABLE
+            }
+            auto_pointer_layer_timer = timer_read();
+        }
     }
     return mouse_report;
 }
